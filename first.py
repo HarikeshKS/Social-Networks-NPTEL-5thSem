c7904e189bb7569c5cdeb48455f10162c6324497
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2023-07-30 14:53:22 +0530

    Learned about importing own py file functions in other file.

---
+++ first.py
@@ -0,0 +1,6 @@
+# Testing python
+import harikesh
+i=0
+while(i<5):
+    print(harikesh.sumnum(harikesh.random.randint(1,100),harikesh.random.randint(1,100),harikesh.random.randint(1,100)))
+    i+=1
---
+++ harikesh.py
@@ -0,0 +1,3 @@
+import random
+def sumnum(a,b,c):
+    return (a+b+c)
